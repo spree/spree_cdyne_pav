166f3b607a1712e9b86f97817f6367d56354193c
Sean O'Hara <sohara@sohara.com>
2013-07-17 12:23:40 -0400

    Our custom order state is 'shipping', replacing 'address'

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -3,7 +3,7 @@ Spree::CheckoutController.class_eval do
   alias_method :orig_update, :update
 
   def update
-    if @order.state == "address"
+    if @order.state == "shipping"
       object_params[:ship_address_attributes][:is_shipping] = true if object_params[:ship_address_attributes]
     end
     orig_update
